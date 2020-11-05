cb2789fdb9d16cc17a658c3731e4d6c539d300c5
merail <rail.mesherov@yandex.ru>
2020-11-06 00:19:53 +0300

    add GA

--- app/src/main/java/com/example/loans/MainActivity.java
+++ app/src/main/java/com/example/loans/MainActivity.java
@@ -9,21 +9,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "onComplete";
 
-    private FirebaseAnalytics firebaseAnalytics;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        firebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         FirebaseMessaging.getInstance().getToken()
                 .addOnCompleteListener(new OnCompleteListener<String>() {
@@ -39,5 +34,19 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(TAG, msg);
                     }
                 });
+
+
+//        Tracker t = ((AnalyticsApplication) this.getApplication()).getDefaultTracker(
+//                TrackerName.APP_TRACKER);
+//
+//        t.setScreenName(screenName);
+//        String campaignData = "http://examplepetstore.com/index.html?" +
+//                "utm_source=email&utm_medium=email_marketing&utm_campaign=summer" +
+//                "&utm_content=email_variation_1";
+//
+//        t.send(new HitBuilders.ScreenViewBuilder()
+//                .setCampaignParamsFromUrl(campaignData)
+//                .build()
+//        );
     }
 }
