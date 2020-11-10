4686f87f10e84c2d1b3fc5ad3683d2ec99436dbb
merail <rail.mesherov@yandex.ru>
2020-11-11 02:37:42 +0300

    design for loans

--- app/src/main/java/com/example/loans/LoansAdapter.java
+++ app/src/main/java/com/example/loans/LoansAdapter.java
@@ -32,7 +32,7 @@ public class LoansAdapter extends RecyclerView.Adapter<LoansAdapter.LoansHolder>
 
     @Override
     public int getItemCount() {
-        return 1;
+        return 2;
     }
 
     static class LoansHolder extends RecyclerView.ViewHolder {
