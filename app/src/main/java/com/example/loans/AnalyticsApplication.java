7d21183afa327999eccd3b4b9620b47abe190aba
merail <rail.mesherov@yandex.ru>
2020-11-12 23:51:03 +0300

    add mytracker and gaid

--- app/src/main/java/com/example/loans/AnalyticsApplication.java
+++ app/src/main/java/com/example/loans/AnalyticsApplication.java
@@ -2,26 +2,13 @@ package com.example.loans;
 
 import android.app.Application;
 
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.Tracker;
+import com.my.tracker.MyTracker;
 
 public class AnalyticsApplication extends Application {
-
-    private static GoogleAnalytics sAnalytics;
-    private static Tracker sTracker;
-
     @Override
     public void onCreate() {
         super.onCreate();
 
-        sAnalytics = GoogleAnalytics.getInstance(this);
-    }
-
-    synchronized public Tracker getDefaultTracker() {
-        if (sTracker == null) {
-            sTracker = sAnalytics.newTracker(R.xml.global_tracker);
-        }
-
-        return sTracker;
+        MyTracker.initTracker("77704704188920986930", this);
     }
 }
--- app/src/main/java/com/example/loans/SplashActivity.java
+++ app/src/main/java/com/example/loans/SplashActivity.java
@@ -1,7 +1,6 @@
 package com.example.loans;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -10,12 +9,20 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.ads.identifier.AdvertisingIdClient;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+
+import java.io.IOException;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getUIDs();
 
         final boolean hasConnection = isNetworkAvailable();
         Handler h = new Handler();
@@ -32,5 +39,23 @@ public class SplashActivity extends AppCompatActivity {
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    void getUIDs() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    AdvertisingIdClient.Info adInfo = AdvertisingIdClient.getAdvertisingIdInfo(getApplicationContext());
+                    String advertisingId = adInfo != null ? adInfo.getId() : null;
+                    Log.d("aaaaaaaaaaa", advertisingId);
+                } catch (IOException | GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        };
+
+        // call thread start for background process
+        thread.start();
     }
 }
