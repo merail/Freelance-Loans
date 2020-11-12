26af61f2b2873788974378f19338aa5064e7aabb
merail <rail.mesherov@yandex.ru>
2020-11-13 00:09:19 +0300

    add appmetrica

--- app/src/main/java/com/example/loans/AnalyticsApplication.java
+++ app/src/main/java/com/example/loans/AnalyticsApplication.java
@@ -3,11 +3,17 @@ package com.example.loans;
 import android.app.Application;
 
 import com.my.tracker.MyTracker;
+import com.yandex.metrica.YandexMetrica;
+import com.yandex.metrica.YandexMetricaConfig;
 
 public class AnalyticsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        YandexMetricaConfig config = YandexMetricaConfig.newConfigBuilder("3698e262-68c3-4352-8926-8d61610c9db8").build();
+        YandexMetrica.activate(getApplicationContext(), config);
+        YandexMetrica.enableActivityAutoTracking(this);
 
         MyTracker.initTracker("77704704188920986930", this);
     }
