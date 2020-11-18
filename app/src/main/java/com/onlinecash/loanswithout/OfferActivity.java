d265a8e7b4a6aaed14c1d87e9803bd3c573f34d9
merail <rail.mesherov@yandex.ru>
2020-11-18 15:08:36 +0300

    end offer activity

--- app/src/main/java/com/onlinecash/loanswithout/OfferActivity.java
+++ app/src/main/java/com/onlinecash/loanswithout/OfferActivity.java
@@ -1,24 +1,34 @@
 package com.onlinecash.loanswithout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Text;
+
 public class OfferActivity extends AppCompatActivity {
+
+    private int money = 17500;
+    private int time = 77;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer);
 
-        TextView moneyProgressTextView = findViewById(R.id.moneyProgressTextView);
+        TextView sumToReturnTextView = findViewById(R.id.sumToReturnTextView);
 
+        TextView moneyProgressTextView = findViewById(R.id.moneyProgressTextView);
         CircularSeekBar moneySeekBar = findViewById(R.id.moneySeekBar);
         moneySeekBar.setOnSeekBarChangeListener(new CircularSeekBar.OnCircularSeekBarChangeListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onProgressChanged(CircularSeekBar circularSeekBar, int progress, boolean fromUser) {
-                moneyProgressTextView.setText(String.valueOf((int) (5000 + (double) progress / 100 * 25000)));
+                money = (int) (5000 + (double) progress / 100 * 25000);
+                moneyProgressTextView.setText(money + " руб.");
+                sumToReturnTextView.setText(String.valueOf((int)(time * 0.001 * money + money)));
             }
 
             @Override
@@ -35,9 +45,12 @@ public class OfferActivity extends AppCompatActivity {
         TextView timeProgressTextView = findViewById(R.id.timeProgressTextView);
         CircularSeekBar timeSeekBar = findViewById(R.id.timeSeekBar);
         timeSeekBar.setOnSeekBarChangeListener(new CircularSeekBar.OnCircularSeekBarChangeListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onProgressChanged(CircularSeekBar circularSeekBar, int progress, boolean fromUser) {
-                timeProgressTextView.setText(String.valueOf((int)(3 + (double) progress / 100 * 27)));
+                time = (int) (61 + (double) progress / 100 * 32);
+                timeProgressTextView.setText(time + " дней");
+                sumToReturnTextView.setText(String.valueOf((int)(time * 0.001 * money + money)));
             }
 
             @Override
