5a6ccf60a0607a841e76c4feef62697a740b828b
merail <rail.mesherov@yandex.ru>
2020-12-04 03:00:24 +0300

    load photo from gallery or camera

--- app/src/main/java/com/onlinecash/loanswithout/RegistrationActivity.java
+++ app/src/main/java/com/onlinecash/loanswithout/RegistrationActivity.java
@@ -3,7 +3,7 @@ package com.onlinecash.loanswithout;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
-import android.content.ActivityNotFoundException;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,6 +11,10 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -28,17 +32,32 @@ import androidx.core.app.ActivityCompat;
 import com.my.tracker.MyTracker;
 import com.yandex.metrica.YandexMetrica;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private static final int INPUT_FILE_REQUEST_CODE = 1;
+    private static final int FILECHOOSER_RESULTCODE = 1;
+    private static final String TAG = MainActivity.class.getSimpleName();
     private static final int PICK_FROM_GALLERY = 2;
     private static final String EXTRA_ORDER = "ORDER";
     private static final String EXTRA_ITEM_ID = "item_id";
     private final static int FILE_CHOOSER_RESULT_CODE = 1;
+    private ValueCallback<Uri> mUploadMessage;
+    private Uri mCapturedImageURI = null;
+    private ValueCallback<Uri[]> mFilePathCallback;
+    private String mCameraPhotoPath;
     private ProgressBar webProgressBar;
     private WebChromeClient webChromeClient;
     private ValueCallback<Uri[]> uploadMessage;
+
+    private WebView bufWebView;
+    private ValueCallback<Uri[]> bufFilePath;
+    private WebChromeClient.FileChooserParams bufFileChooserParams;
 
     private WebView chooserWebView;
     private ValueCallback<Uri[]> chooserFilePathCallback;
@@ -58,6 +77,9 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        //       , Manifest.permission.READ_EXTERNAL_STORAGE}, PICK_FROM_GALLERY);
 
         SharedPreferences sharedPreferences = getSharedPreferences("PREFS", Context.MODE_PRIVATE);
         String facebookDeepLink = sharedPreferences.getString("facebook_deep_link", "not_available");
@@ -96,71 +118,114 @@ public class RegistrationActivity extends AppCompatActivity {
         });
 
         webChromeClient = new WebChromeClient() {
-            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
+            // For Android 5.0
+            public boolean onShowFileChooser(WebView view, ValueCallback<Uri[]> filePath, WebChromeClient.FileChooserParams fileChooserParams) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(RegistrationActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE
+                            , Manifest.permission.WRITE_EXTERNAL_STORAGE}, PICK_FROM_GALLERY);
 
-                    chooserWebView = mWebView;
-                    chooserFilePathCallback = filePathCallback;
-                    chooserFileChooserParams = fileChooserParams;
+                    bufWebView = view;
+                    bufFilePath = filePath;
+                    bufFileChooserParams = fileChooserParams;
 
-                    ActivityCompat.requestPermissions(RegistrationActivity.this,
-                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PICK_FROM_GALLERY);
-
-                    return true;
-                } else {
-                    if (uploadMessage != null) {
-                        uploadMessage.onReceiveValue(null);
-                        uploadMessage = null;
-                    }
-
-                    uploadMessage = filePathCallback;
-                    Intent intent = null;
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        intent = fileChooserParams.createIntent();
-                    }
-                    try {
-                        startActivityForResult(intent, FILE_CHOOSER_RESULT_CODE);
-                    } catch (ActivityNotFoundException e) {
-                        uploadMessage = null;
-                        return false;
-                    }
                     return true;
                 }
+                // Double check that we don't have any existing callbacks
+                if (mFilePathCallback != null) {
+                    mFilePathCallback.onReceiveValue(null);
+                }
+                mFilePathCallback = filePath;
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                        takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath);
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                        Log.e(TAG, "Unable to create Image File", ex);
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        mCameraPhotoPath = "file:" + photoFile.getAbsolutePath();
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                Uri.fromFile(photoFile));
+                        takePictureIntent.putExtra("INTENT_TYPE", "takePictureIntent");
+                    } else {
+                        takePictureIntent = null;
+                    }
+                }
+                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                contentSelectionIntent.setType("image/*");
+                contentSelectionIntent.putExtra("INTENT_TYPE", "contentSelectionIntent");
+                Intent[] intentArray;
+                if (takePictureIntent != null) {
+                    intentArray = new Intent[]{takePictureIntent};
+                } else {
+                    intentArray = new Intent[0];
+                }
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+                startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
+                return true;
             }
+
+            // openFileChooser for Android 3.0+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
+                mUploadMessage = uploadMsg;
+                // Create AndroidExampleFolder at sdcard
+                // Create AndroidExampleFolder at sdcard
+                File imageStorageDir = new File(
+                        Environment.getExternalStoragePublicDirectory(
+                                Environment.DIRECTORY_PICTURES)
+                        , "AndroidExampleFolder");
+                if (!imageStorageDir.exists()) {
+                    // Create AndroidExampleFolder at sdcard
+                    imageStorageDir.mkdirs();
+                }
+                // Create camera captured image file path and name
+                File file = new File(
+                        imageStorageDir + File.separator + "IMG_"
+                                + System.currentTimeMillis()
+                                + ".jpg");
+                mCapturedImageURI = Uri.fromFile(file);
+                // Camera capture image intent
+                final Intent captureIntent = new Intent(
+                        android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                // Create file chooser intent
+                Intent chooserIntent = Intent.createChooser(i, "Image Chooser");
+                // Set camera intent to file chooser
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS
+                        , new Parcelable[]{captureIntent});
+                // On select image call onActivityResult method of activity
+                startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+            }
+
+            // openFileChooser for Android < 3.0
+            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                openFileChooser(uploadMsg, "");
+            }
+
+            //openFileChooser for other Android versions
+            public void openFileChooser(ValueCallback<Uri> uploadMsg,
+                                        String acceptType,
+                                        String capture) {
+                openFileChooser(uploadMsg, acceptType);
+            }
+
+
         };
+
         webView.setWebChromeClient(webChromeClient);
-        new WebChromeClient() {
-            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(RegistrationActivity.this,
-                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE
-                            , Manifest.permission.WRITE_EXTERNAL_STORAGE
-                            , Manifest.permission.CAMERA}, PICK_FROM_GALLERY);
-                    return false;
-                } else {
-                    if (uploadMessage != null) {
-                        uploadMessage.onReceiveValue(null);
-                        uploadMessage = null;
-                    }
-
-                    uploadMessage = filePathCallback;
-                    Intent intent = null;
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        intent = fileChooserParams.createIntent();
-                    }
-                    try {
-                        startActivityForResult(intent, FILE_CHOOSER_RESULT_CODE);
-                    } catch (ActivityNotFoundException e) {
-                        uploadMessage = null;
-                        return false;
-                    }
-                    return true;
-                }
-            }
-        };
-
 
         if (Utils.isNetworkAvailable(getApplicationContext())) {
             String link = order + "&aff_sub1=" + Utils.instanceId[0]
@@ -191,27 +256,55 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_GRANTED)
-                webChromeClient.onShowFileChooser(chooserWebView, chooserFilePathCallback, chooserFileChooserParams);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode != INPUT_FILE_REQUEST_CODE || mFilePathCallback == null) {
+                super.onActivityResult(requestCode, resultCode, data);
+                return;
+            }
+            Uri[] results = null;
+            // Check that the response is a good one
+            if (resultCode == Activity.RESULT_OK) {
+                if (data == null) {
+                    // If there is not data, then we may have taken a photo
+                    if (mCameraPhotoPath != null) {
+                        results = new Uri[]{Uri.parse(mCameraPhotoPath)};
+                    }
+                } else {
+                    if (data.getFlags() == 0) {
+                        results = new Uri[]{Uri.parse(mCameraPhotoPath)};
+                        mCameraPhotoPath = null;
+                    } else {
+                        String dataString = data.getDataString();
+                        if (dataString != null) {
+                            results = new Uri[]{Uri.parse(dataString)};
+                        }
+                    }
+                }
+            }
+            mFilePathCallback.onReceiveValue(results);
+            mFilePathCallback = null;
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        if (requestCode == FILE_CHOOSER_RESULT_CODE) {
-            if (null == uploadMessage) return;
-            String result = intent == null || resultCode != RESULT_OK ? null
-                    : intent.getDataString();
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+    }
 
-            uploadMessage.onReceiveValue(new Uri[]{Uri.parse(result)});
-            uploadMessage = null;
-        }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            webChromeClient.onShowFileChooser(bufWebView, bufFilePath, bufFileChooserParams);
     }
 }
